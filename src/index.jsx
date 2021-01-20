9014b970e327c1e421edd258afbbaf529b58c9e4
SvitlanaReutska <lana.reutska@gmail.com>
2021-01-20 16:48:37 +0200

    feat: create  url request

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,64 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+
+class MyComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: []
+    };
+  }
+
+  componentDidMount() {
+    fetch("http://localhost:3000/api/goods")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            items: result.result
+          });
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+  }
+
+  render() {
+    const { error, isLoaded, items } = this.state;
+    if (error) {
+      return <div>Ошибка: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Загрузка...</div>;
+    } else {
+      return (
+        <ul>
+          {items.map(item => (
+            <li>
+              {item.title} {item.price}
+              <img src={item.img}></img>
+            </li>
+          ))}
+        </ul>
+      );
+    }
+  }
+}
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <MyComponent />
   </React.StrictMode>,
   document.getElementById('root')
 );
